eb277120c80a163f2b307e1c4d8f8d0f0846ddea
Vladislav <wladgarshko@gmail.com>
2022-08-02 18:16:03 +0300

    for deploy3

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const jsonServer = require('json-server');
+const auth  = require('json-server-auth')
+const cors  = require('cors')
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
@@ -6,9 +8,20 @@ const middlewares = jsonServer.defaults({
 });
 const PORT  = process.env.PORT || 8080;
 server.use(middlewares);
+
+server.use(cors())
 server.use(jsonServer.rewriter({
     '/api/*' : '/$1',
 }));
+const rules = auth.rewriter({
+    user: 600,
+    messages: 640
+})
+
+server.db = router.db
+
+server.use(rules)
+server.use(auth)
 server.use(router);
 server.listen(PORT, () => {
     console.log('Server is running');
